39292470fad8fd1bc4c19d6ad738ef5cfc129294
Olivier Poitrey <rs@dailymotion.com>
2011-07-10 16:50:34 +0200

    Check Safari API compatibility to gracefully fail in case of future incompatible Safari API change

--- SafariTabSwitching/SafariTabSwitching.m
+++ SafariTabSwitching/SafariTabSwitching.m
@@ -17,16 +17,24 @@
         && event.keyCode >= 18 && event.keyCode <= 25)
     {
         NSUInteger tabIndex = event.keyCode - 18;
-        NSWindow *frontWindow = [[NSApplication sharedApplication] frontWindow];
-        if ([[frontWindow orderedTabViewItems] count] >= (tabIndex + 1))
+        if ([[NSApplication sharedApplication] respondsToSelector:@selector(frontWindow)]) // check safari API compat
         {
-            [frontWindow setCurrentTabViewItem:[[frontWindow orderedTabViewItems] objectAtIndex:tabIndex]];
+            NSWindow *frontWindow = [[NSApplication sharedApplication] performSelector:@selector(frontWindow)];
+
+            if ([frontWindow respondsToSelector:@selector(orderedTabViewItems)] // check safari API compat
+                && [frontWindow respondsToSelector:@selector(setCurrentTabViewItem:)])
+            {
+                NSArray *tabs = [frontWindow performSelector:@selector(orderedTabViewItems)];
+                if (tabs.count >= (tabIndex + 1))
+                {
+                    [frontWindow performSelector:@selector(setCurrentTabViewItem:) withObject:[tabs objectAtIndex:tabIndex]];
+                    return; // prevent event dispatching
+                }
+            }
         }
     }
-    else
-    {
-        [self SafariTabeSwitching_sendEvent:event];
-    }
+
+    [self SafariTabeSwitching_sendEvent:event];
 }
 
 @end
